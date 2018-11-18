cf8d2ceb6cb78b3863e298207500f51ba7bfb4f5
MyVueAdmin <45121404+MyVueAdmin@users.noreply.github.com>
2018-11-18 13:15:59 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+function __autoload ($class) {require_once("classes/$class.php");}
+
+$ini = "config.ini"; // path to .ini-file
+
+$config = parse_ini_file($ini, true);
+
+$authorizer = new Authorizer(
+	$config["security"]["method"],
+	$config["security"]["key"],
+	$config["security"]["salt"],
+	$config["security"]["expire"]
+);
+
+$driver = new Driver();
+$driver->headers(
+	$config["security"]["clients"],
+	$config["settings"]["timezone"],
+	$config["settings"]["errors"]
+);
+
+echo json_encode($driver->execute(
+	$config["settings"]["host"],
+	$authorizer
+));
